21eb97bce400debe2dea445aaa182336e2c706ca
Afroz Sorathiya <afrozhusenbhai@gmail.com>
2020-10-23 12:33:40 -0700

    design setup

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- FontAwesome Css -->
+    <link rel="stylesheet" href="css/all.min.css">
+    <title>E-Learning</title>
+    <style>
+    .fa-school {
+        color: gold;
+    }
+
+    img {
+        height: -webkit-fill-available;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <header class="sticky-top">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    <i class="fa fa-school"></i>
+                    <span class="px-2">E-Learning</span>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="#">Pricing</a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <div class="row">
+            <div class="col-12">
+                <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
+                    </ol>
+                    <div class="carousel-inner" style="height:350px">
+                        <div class="carousel-item active">
+                            <img src="assets/images/andy-falconer-qADcG-2qMd4-unsplash.jpg" class="d-block w-100"
+                                alt="...">
+                            <div class="carousel-caption">
+                                <h5>First slide label</h5>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="assets/images/jeshoots-com--2vD8lIhdnw-unsplash.jpg" class="d-block w-100"
+                                alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="assets/images/john-schnobrich-2FPjlAyMQTA-unsplash.jpg" class="d-block w-100"
+                                alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="assets/images/scott-graham-5fNmWej4tAA-unsplash.jpg" class="d-block w-100"
+                                alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="assets/images/thomas-park-6MePtA9EVDA-unsplash.jpg" class="d-block w-100"
+                                alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <footer class="row bg-dark mx-0 p-3">
+            <div class="col-md-6 text-justify my-3" style="color:white">
+                <h4>About</h4>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem voluptatibus omnis
+                fugit voluptatum pariatur? Unde corrupti ut quo sunt porro distinctio, nihil reprehenderit culpa facere
+                non in nisi ducimus?
+            </div>
+            <div class="col-md-6 text-justify my-3" style="color:white">
+                <h4>About</h4>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem voluptatibus omnis
+                fugit voluptatum pariatur? Unde corrupti ut quo sunt porro distinctio, nihil reprehenderit culpa facere
+                non in nisi ducimus?
+            </div>
+            <div class="col-md-6 text-justify my-3" style="color:white">
+                <h4>About</h4>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem voluptatibus omnis
+                fugit voluptatum pariatur? Unde corrupti ut quo sunt porro distinctio, nihil reprehenderit culpa facere
+                non in nisi ducimus?
+            </div>
+        </footer>
+    </div>
+</body>
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/all.min.js"></script>
+
+</html>
